b2bcff03ab3cfceb70bae4840cc5d127c0d7a4b8
RegisLewis <regisaslewis@gmail.com>
2023-02-03 15:30:29 -0800

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, name) {
+    return array.filter(function (inp) {
+        return (inp === name || inp === name.toUpperCase() || inp === name.toLowerCase());
+    })
+}
+
+function fuzzyMatch(array, firstTwo) {
+    return array.filter(function (inp) {
+        return firstTwo === (inp[0] + inp[1]);
+    })
+}
+
+function matchName(array, driverName) {
+    return array.filter(function (inp) {
+      return inp.name === driverName;
+    })
+  }
